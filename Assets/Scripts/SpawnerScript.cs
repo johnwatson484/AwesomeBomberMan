3f9302a63e9c38b13eb3ff818640024bc667d4b4
John Watson <john.watson1@defra.gov.uk>
2025-04-24 21:02:55 +0100

    Add bombs

---
+++ Assets/Scripts/SpawnerScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using UnityEngine;
+
+public class SpawnerScript : MonoBehaviour
+{
+    public GameObject bombPrefab;
+    private float minX = -2.55f;
+    private float maxX = 2.55f;
+
+    void Start()
+    {
+        StartCoroutine(SpawnBombs());
+    }
+
+    IEnumerator SpawnBombs()
+    {
+        yield return new WaitForSeconds(Random.Range(0f, 1f));
+
+        Instantiate(bombPrefab, new Vector2(Random.Range(minX, maxX), transform.position.y), Quaternion.identity);
+
+        StartCoroutine(SpawnBombs());
+    }
+}
